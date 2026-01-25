3e8f489972f4fcf8d02689f735ce5bb62814216f
Wookie3 <cbsbjb@gmail.com>
2026-01-25 17:40:27 -0500

    fix: Fix dialog positioning and remove transparency

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -50,9 +50,15 @@ function DialogContent({
       <DialogPrimitive.Content
         data-slot="dialog-content"
         className={cn(
-          "fixed left-1/2 top-1/2 z-50 -translate-x-1/2 -translate-y-1/2 w-full max-w-xl p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95",
+          "z-50 w-full max-w-xl max-h-[calc(100vh-4rem)] overflow-y-auto p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 bg-background",
           className
         )}
+        style={{
+          position: 'fixed',
+          left: '50%',
+          top: '2rem',
+          transform: 'translateX(-50%)',
+        }}
         {...props}
       >
         {children}
